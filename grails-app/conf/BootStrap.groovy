05ce87fc62c2b608a2d3478b8cbea998b2cb9ab8
希望的石头CUP <xplixina@sina.com>
2015-12-28 17:37:46 +0800

    增加了一个域类

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+
+import test.NewDomainClass
+
 class BootStrap {
 
     def init = { servletContext ->
+        environments {
+            development {
+                for (int i=0; i<15; i++) {
+                    def n = new NewDomainClass(name: "张${i}")
+                    n.save()
+                }
+            }
+        }
     }
     def destroy = {
     }
